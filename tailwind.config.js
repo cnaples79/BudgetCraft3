9b18b617cfe85cbcbb974e35b81272e27f7daf3d
Chase Naples <cnaples79@gmail.com>
2024-12-11 23:18:54 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms';
 export default {
   content: [
     "./index.html",
@@ -8,6 +9,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    forms,
   ],
 }
